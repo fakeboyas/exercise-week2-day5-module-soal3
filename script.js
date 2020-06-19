6f9a4e254bec967b8e5bd8ce48191f6954b33fe8
fakeboyas <ovoivi9@gmail.com>
2020-06-19 16:51:50 +0700

    fix Bug

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 import { changeCase } from "./soal3.js";
 
-let btn1 = document.getElementById("btn1");
-btn1.addEventListener("click", changeCase());
+changeCase();
--- soal3.js
+++ soal3.js
@@ -1,13 +1,14 @@
 function changeCase() {
-    let testUpper = /[A-Z]/;
-    let input = document.getElementById("inputText").value;
-    if (testUpper.test(input)) {
-        document.getElementById("display").innerHTML = input.toLowerCase();
-    } else if (testUpper.test(input)) {
-        document.getElementById("display").innerHTML = input.toLowerCase();
-    } else {
-        document.getElementById("display").innerHTML = input.toUpperCase();
-    }
+    let btn1 = document.getElementById("btn1");
+    btn1.addEventListener("click", function () {
+        let input = document.getElementById("inputText").value;
+        let testUpper = /[A-Z]/;
+        if (testUpper.test(input)) {
+            document.getElementById("display").innerHTML = input.toLowerCase();
+        } else {
+            document.getElementById("display").innerHTML = input.toUpperCase();
+        }
+    });
 }
 
 export { changeCase };
